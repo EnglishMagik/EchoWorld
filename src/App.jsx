6efc8af57bd88a9851e33b2938518165bc96dcee
EnglishMagik <chantelhere@gmail.com>
2026-04-16 21:58:05 +0200

    fix: layout and button sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-import Scene from "./components/Scene"
-import MicButton from "./components/MicButton"
-import TranscriptBox from "./components/TranscriptBox"
-import FeedbackPanel from "./components/FeedbackPanel"
+import Scene from "./components/Scene";
+import MicButton from "./components/MicButton";
+import TranscriptBox from "./components/TranscriptBox";
+import FeedbackPanel from "./components/FeedbackPanel";
 
-import "./styles/main.css"
+import LearnerDashboard from "./components/LearnerDashboard";
+import TeachingMindDashboard from "./components/TeachingMindDashboard";
+import SpeechPanel from "./components/SpeechPanel";
+
+import "./styles/main.css";
 
 export default function App() {
   return (
-    <div className="app">
-      <Scene />
-      
-      <div className="ui-layer">
-        <MicButton />
-        <TranscriptBox />
-        <FeedbackPanel />
+    <div className="app-layout">
+
+      {/* TOP BAR */}
+      <div className="top-bar">
+        <button className="scenario">Scenario Library</button>
+        <p>Mood: calm</p>
+        <p>Scene: default</p>
+        <p>Session: 0</p>
       </div>
+
+      {/* CENTER */}
+      <div className="main-area">
+        <div>
+          <h1>Learning Session</h1>
+          <Scene />
+          <TranscriptBox />
+          <MicButton />
+        </div>
+      </div>
+
+      {/* LEFT */}
+      <div className="left-panel">
+        <LearnerDashboard />
+      </div>
+
+      {/* RIGHT */}
+      <div className="right-panel">
+        <TeachingMindDashboard />
+        <FeedbackPanel />
+        <SpeechPanel />
+      </div>
+
     </div>
-  )
+  );
 }
